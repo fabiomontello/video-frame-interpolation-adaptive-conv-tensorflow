f4f7631decb9208e9103164b1f2afb65e0db5153
fabiomontello <montellofabio@gmail.com>
2020-04-12 13:35:45 +0200

    Update datasetCreationUtils.py

--- datasetCreationUtils.py
+++ datasetCreationUtils.py
@@ -1,9 +1,49 @@
-import tensorflow as tf
 import cv2
 import os
 import random
 import numpy as np
 import pandas as pd
+
+# Global counting of the data
+folder_count = 0
+frame_lab = ['a', 'b', 'c']
+
+# For each video in the list, extract some frames
+def extractFromVideos(lst):
+  os.mkdir(PATH)
+  for elem in lst:
+    extractFrames("/content/Hollywood2/AVIClips/"+elem, PATH)
+    print(elem)
+ 
+# At each video passed, extract 3 frames every 10, and grouping them in a subfolder
+def extractFrames(pathIn, pathOut):
+    FPS = 25
+ 
+    cap = cv2.VideoCapture(pathIn)
+    count = 0
+ 
+    global folder_count
+ 
+    rel_path = pathOut
+    os.makedirs(pathOut, exist_ok=True)
+    while (cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+ 
+        if ret == True:
+            if(count%FPS == 0 or count%FPS == 1 or count%FPS == 2):
+              if(count%FPS == 0 ):
+                # final_path = rel_path+"{:d}/".format(folder_count)
+ 
+                folder_count = int(folder_count) + 1
+              cv2.imwrite(os.path.join(pathOut, "frame"+str(folder_count)+frame_lab[count%FPS]+".jpg"), frame)  # save frame as JPEG file
+            count += 1
+        else:
+            break
+ 
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 #function to crop a square image of 150 x 150 around a given pixel
 def crop_image(path, cx, cy):
